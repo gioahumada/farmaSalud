a8854b8f9fa2aa6daa6215b3062512f9a8c0a9be
Giovanni Ahumada <giovanni.ahumada.t@gmail.com>
2024-06-01 21:06:19 -0400

    chore: Add menu functionality to FarmaSalud

--- splitVer/main.c
+++ splitVer/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "menu.h"
+
+
 /* Definir el máximo de productos por cliente */
 #define MAX_PRODUCTOS_POR_CLIENTE 10000
 
@@ -926,90 +929,6 @@ void transferirProductosProveedorASucursal(struct FarmaSalud *farmacia) {
 
     pause();
     cls();
-}
-
-
-
-void menuPrincipal(struct FarmaSalud *farmacia) {
-    int opcion;
-    do {
-        printf("\n--- FarmaSalud ---\n");
-        printf("1. leerClientes\n");
-        printf("2. crearCliente\n");
-        printf("3. eliminarCliente\n");
-        printf("4. agregarProductoACliente\n");
-        printf("5. leerSucursales\n");
-        printf("6. crearSucursal\n");
-        printf("7. eliminarSucursal\n");
-        printf("8. crearProveedor\n");
-        printf("9. leerProveedor\n");
-        printf("10. eliminarProveedor\n");
-        printf("11. agregarProductoProveedor\n");
-        printf("12. transferirProductosProveedorASucursal\n");
-
-
-        printf("\n13. Salir\n");
-        printf("\nSeleccione una opcion: ");
-        scanf("%d", &opcion);
-
-        switch (opcion) {
-            case 1:
-                leerClientes(farmacia);
-                cls();
-                break;
-            case 2:
-                agregarCliente(farmacia);
-                cls();
-                break;
-            case 3:
-                eliminarCliente(farmacia);
-                cls();
-                break;
-            case 4:
-                agregarProductoACliente(farmacia);
-                cls();
-                break;
-            case 5:
-                leerSucursales(farmacia);
-                cls();
-                break;
-            case 6:
-                crearSucursal(farmacia);
-                cls();
-                break;
-            case 7:
-                eliminarSucursal(farmacia);
-                cls();
-                break;
-            case 8:
-                crearProveedor(farmacia);
-                cls();
-                break;
-            case 9:
-                leerProveedores(farmacia);
-                cls();
-                break;
-            case 10:
-                eliminarProveedor(farmacia);
-                cls();
-                break;
-            case 11:
-                agregarProductoProveedor(farmacia);
-                cls();
-                break;
-            case 12:
-                transferirProductosProveedorASucursal(farmacia);
-                cls();
-                break;
-            case 13:
-                printf("Saliendo...\n");
-                exit(0);
-                break;
-            default:
-                printf("Opcion no valida. Intente nuevamente.\n");
-                break;
-        }
-    } while (opcion != 13);
 }
 
 int main() 
