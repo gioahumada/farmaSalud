c787e5cfff631287607788fc99c7456c3d13d073
Patricio Hernandez <patricio.hernandez.v@mail.pucv.cl>
2024-06-08 23:35:31 -0400

    Update crearSucursal.h
    Funcion hecha por Simon

--- TurboC/crearSucursal.h
+++ TurboC/crearSucursal.h
@@ -1,19 +1,31 @@
-void crearSucursal(struct FarmaSalud *farmacia) {
+void agregarSucursal(struct FarmaSalud *farmacia, struct NodoSucursales *nuevaSucursal) {
+    if (farmacia->sucursales == NULL) {
+        farmacia->sucursales = nuevaSucursal;
+        nuevaSucursal->ant = nuevaSucursal->sig = nuevaSucursal;
+    } else {
+        struct NodoSucursales *ultimo = farmacia->sucursales->ant;
+        ultimo->sig = nuevaSucursal;
+        nuevaSucursal->ant = ultimo;
+        nuevaSucursal->sig = farmacia->sucursales;
+        farmacia->sucursales->ant = nuevaSucursal;
+    }
+}
+
+void leerDatosSucursal(struct Sucursal *nuevaSucursal) {
     cls();
-    struct NodoSucursales *nuevoNodo = (struct NodoSucursales*)malloc(sizeof(struct NodoSucursales));
-    struct Sucursal *nuevaSucursal = (struct Sucursal*)malloc(sizeof(struct Sucursal));
-
-    nuevoNodo->datosSucursal = nuevaSucursal;
-    nuevoNodo->ant = nuevoNodo->sig = NULL;
-
     printf("Ingrese ID de la sucursal: ");
     scanf("%d", &nuevaSucursal->id);
-    printf("Ingrese nombre de la sucursal: ");
+    getchar(); // Limpiar el buffer de entrada después de leer un entero
+
     nuevaSucursal->nombre = (char *)malloc(50 * sizeof(char));
-    scanf("%s", nuevaSucursal->nombre);
-    printf("Ingrese dirección de la sucursal: ");
+    printf("Ingrese nombre de la sucursal: ");
+    scanf(" %[^\n]", nuevaSucursal->nombre);
+    getchar(); // Limpiar el buffer de entrada después de leer un string
+
     nuevaSucursal->direccion = (char *)malloc(100 * sizeof(char));
-    scanf("%s", nuevaSucursal->direccion);
+    printf("Ingrese dirección de la sucursal: ");
+    scanf(" %[^\n]", nuevaSucursal->direccion);
+    getchar(); // Limpiar el buffer de entrada después de leer un string
 
     nuevaSucursal->cantidadDeVentas = 0; // Inicializar a 0
     nuevaSucursal->capacidadAlmacenamiento = 0; // Cambiar si se necesita un valor específico
@@ -23,8 +35,17 @@ void crearSucursal(struct FarmaSalud *farmacia) {
     for (int i = 0; i < MAX_ENVIOS; i++) {
         nuevaSucursal->registrosEnvios[i] = NULL; // Inicializar a NULL
     }
+}
 
-    agregarSucursalConsole(farmacia, nuevoNodo);
+void crearSucursal(struct FarmaSalud *farmacia) {
+    struct NodoSucursales *nuevoNodo = (struct NodoSucursales*)malloc(sizeof(struct NodoSucursales));
+    struct Sucursal *nuevaSucursal = (struct Sucursal*)malloc(sizeof(struct Sucursal));
+
+    nuevoNodo->datosSucursal = nuevaSucursal;
+    nuevoNodo->ant = nuevoNodo->sig = NULL;
+
+    leerDatosSucursal(nuevaSucursal);
+    agregarSucursal(farmacia, nuevoNodo);
 
     cls();
     printf("Sucursal agregada con éxito.\n");
